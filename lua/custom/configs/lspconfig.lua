72e4185e58b3154ecde312ac78ec7eac4ff59c28
jp-roisin <72938245+jp-roisin@users.noreply.github.com>
2024-06-14 11:34:16 +0200

    Add eslint setup

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,27 +1,8 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
-
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "solargraph", "texlab" }
+local servers = { "html", "cssls", "eslint", "tsserver", "clangd", "rust_analyzer", "solargraph", "texlab" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+  lspconfig[lsp].setup {}
 end
-
--- on_attach.rust_analyzer.setup{
---   settings = {
---     ['rust-analyzer'] = {
---       diagnostics = {
---         enable = false;
---       }
---     }
---   }
--- }
-
---
--- lspconfig.pyright.setup { blabla}
