db08290041d3d7a966e57eed693fdaf1ce83f4ee
jp-roisin <72938245+jp-roisin@users.noreply.github.com>
2024-07-16 13:57:32 +0200

    Remove Markview

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -165,18 +165,6 @@ local plugins = {
     lazy = false,
   },
 
-  {
-    "OXY2DEV/markview.nvim",
-    dependencies = {
-      -- You may not need this if you don't lazy load
-      -- Or if the parsers are in your $RUNTIMEPATH
-      "nvim-treesitter/nvim-treesitter",
-
-      "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false,
-  },
-
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
